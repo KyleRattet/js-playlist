e77cb071ff335b19d0796c934db9aac3c765185d
KyleRattet <krattet@gmail.com>
2015-08-05 06:26:50 -0600

    first 4 tests pass

--- playlist.js
+++ playlist.js
@@ -1,14 +1,47 @@
 var Song = require('./song');
 
 var Playlist = function(){
+  this.songs = []
 };
 
 Playlist.prototype.isEmpty = function () {
+  if(this.songs.length === 0){
+    return true;
+  }
+  else {
+    return false;
+  }
+  };
+
+Playlist.prototype.addSong = function(song) {
+  this.songs.push(song);
 };
 
-
-Playlist.prototype.forNumber = function () {
-
+Playlist.prototype.songNames = function() {
+  var results = [];
+  for (var i = 0; i < this.songs.length; i++) {
+    results.push(this.songs[i].name);
+  }
+  return results;
 };
 
-module.exports = Playlist;
+Playlist.prototype.removeSong = function (song) {
+  var remove = this.songs.indexOf(song);
+  this.songs.splice(remove, 1);
+};
+
+Playlist.prototype.totalLength = function () {
+  var total = 0;
+
+  for (var i = 0; i < this.songs.length; i++) {
+    total += this.songs[i].length;
+  }
+  return total;
+};
+
+// Playlist.prototype.swap = function (song1, song2) {
+//   // body...
+// };
+
+
+  module.exports = Playlist;
